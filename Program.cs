70a07d41cbd78349f5f35754a315025ff2ad5472
mlememm <yun.viviyet@gmail.com>
2025-06-15 15:23:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace InterfacesTask2
+{
+    interface IConnectable
+    {
+        void Connect();
+    }
+    interface IDisconnectable
+    {
+        void Disconnect();
+    }
+    class Device : IConnectable, IDisconnectable
+    {
+        public void Connect()
+        {
+            Console.WriteLine("Пристрiй пiдключено");
+        }
+        public void Disconnect()
+        {
+            Console.WriteLine("Пристрiй вiдключено");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Device myDevice = new Device();
+            myDevice.Connect();
+            myDevice.Disconnect();
+            Console.ReadLine();
+        }
+    }
+}
